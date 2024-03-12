f65e9535514ff4c89054b6b1f9a423bfe4b1f5df
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2024-03-12 00:22:09 +0000

    Subindo Yaml + teste do get

--- api.test.js
+++ api.test.js
@@ -1,9 +1,9 @@
 const supertest = require('supertest');
-const app = require('./app'); // Importe seu aplicativo Express
+const app = require('./index.js'); // Importe seu aplicativo Express
 
 describe('Teste GET /users', () => {
   it('Get deve retornar 200 em sucesso', async () => {
     const response = await supertest(app).get('/users');
-    expect(response.statusCode).toBe(201);
+    expect(response.statusCode).toBe(200);
   });
 });
